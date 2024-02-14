406ce6a239a496cbfad59f8244260db62741ab52
freecalradia <freecalradia@gmail.com>
2024-02-14 02:21:30 +0100

    - Init

--- KAW_MWdevB.c
+++ KAW_MWdevB.c
@@ -1,5 +1,33 @@
-#include <windows.h>
-int main(void) {
-	printf("\n");
-	return 0;
-}
+//#include <windows.h>
+////
+//int main(void) {
+//	//MessageBoxW(
+//	//	NULL,
+//	//	L"TextMessage",
+//	//	L"TextTitle",
+//	//	MB_YESNOCANCEL
+//	// 
+//	MessageBoxW(NULL, L"Actually my first C-messagebox!", L"Another text!", MB_YESNOCANCEL | MB_ICONEXCLAMATION);
+//	return EXIT_SUCCESS;
+//
+//	//HANDLE CreateRemoteThread(
+//	//	[in]	HANDLE					hProcess,
+//	//	[in]	LPSECURITY_ATTRIBUTES	lpThreadAttributes,
+//	//	[in]	SIZE_T					dwStackSize,
+//	//	[in]	LPTHREAD_START_ROUTINE	lpStartAddress,
+//	//	[in]	LPVOID					lpParameter,
+//	//	[in]	DWORD					dwCreationFlags,
+//	//	[out]	LPWORD					lpThreadId
+//	//);
+//
+//	//HANDLE CreateRemoteThread(
+//	//	[in]			HANDLE							hProcess,
+//	//	[in, optional]	LPSECURITY_ATTRIBUTES			lpThreadAttributes,
+//	//	[in]			SIZE_T							dwStackSize,
+//	//	[in]			LPTHREAD_START_ROUTINE			lpStartAddress,
+//	//	[in, optional]	LPVOID							lpParameter,
+//	//	[in]			DWORD							dwCreationFlags,
+//	//	[in, optional]	LPPROC_THREAD_ATTRIBUTE_LIST	lpAttributeList,
+//	//	[out, optional]	LPWORD							lpThreadId
+//	//);
+//}
